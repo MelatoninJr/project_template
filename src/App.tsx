2ec82c540e1fdab2501f3acf1db595460b2e8a9d
MelatoninJr <dillon32300@gmail.com>
2025-01-07 16:05:53 -0600

    Finish template setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from '@nextui-org/react'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button color="primary">Click me!</Button>
+
+    </h1>
+    </>
+  )
+}
+
+export default App
